1e52d102b4b4095b12fe4966167698491c2bfee9
mevadadarshan19 <mevadadarshan19@users.noreply.github.com>
2021-03-08 07:21:44 +0000

    PalladiumOS Adaptation
    Signed-off-by: mevadadarshan19 <mevadadarshan19@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,11 +1,12 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 The PaladiumOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X01BD.mk
+    $(LOCAL_DIR)/palladium_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X01BD-userdebug
+    palladium_X01BD-userdebug \
+    palladium_X01BD-user
---
+++ palladium_X01BD.mk
@@ -0,0 +1,62 @@
+#
+# Copyright (C) 2020 The PaladiumOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit some common PalladiumOS stuff
+$(call inherit-product, vendor/palladium/config/common_full_phone.mk)
+
+# Blur
+TARGET_USES_BLUR := true
+
+# GAPPS
+ifeq ($(with_gapps), yes)
+PALLADIUM_BUILD_VARIANT := GAPPS
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+endif
+
+#PALLADIUM-PROPS
+PRODUCT_PRODUCT_PROPERTIES += \
+	ro.palladiumdevice.maintainer=Darshan \
+	ro.palladiumdevice.cpu=SDM660 \
+	ro.palladiumdevice.display=6.26 \
+	ro.palladiumdevice.displaytype=IPS LCD PANEL \
+	ro.palladiumdevice.battery=5000mAh \
+	ro.palladiumdevice.camera=12MP+13MP+5MP
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# ZenParts
+$(call inherit-product-if-exists, packages/apps/ZenParts/zenparts.mk)
+
+# FaceUnlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Inherit from X01BD device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := palladium_X01BD
+PRODUCT_DEVICE := X01BD
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := ASUS_X01BDA
+PRODUCT_MANUFACTURER := asus
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE=ASUS_X01BD_1 \
+    PRODUCT_NAME=WW_X01BD \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210505.003 7255357 release-keys"
+
+# Build fingerprint
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys"
